fbd79694e539aff079c717d9ddc8f134a3948a12
Hadassah Uche <uhadassah1@gmail.com>
2023-11-20 20:51:22 +0200

    Add project files.

---
+++ UcheHadassah6_5/HadassahUche6_5.cpp
@@ -0,0 +1,3 @@
+/*Valid names: Write a program that reads a person’s name and checks if the name is 
+valid or not, printing an appropriate message in each case. A valid name can contain 
+letters (i.e. alphabetic characters), humphens (-), apostrophes (’) and spaces only.*/
